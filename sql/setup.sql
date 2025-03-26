ed675e5db5b109a01f5f5858d44d2f1c10533b43
Ian Darwin <ian@darwinsys.com>
2025-03-26 15:07:59 -0400

    More on startup, including DB setup.

---
+++ sql/setup.sql
@@ -0,0 +1,25 @@
+--
+-- Setup for eselling database
+--
+-- If any fields have been added since this was made,
+-- the "update" mode should take care of them.
+--
+
+create table item (
+	id int8 primary key,
+	description varchar not null,
+	sold_date date,
+	sold_for float4,
+	askingprice float8,
+	name varchar not null,
+	soldprice float8,
+	urls _varchar,
+	condition varchar,
+);
+
+create table users (
+	id int8 primary key,
+	name varchar,
+	password varchar,
+	passwordhash varchar,
+);
